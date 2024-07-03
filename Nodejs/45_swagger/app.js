dc59f54043203791816dadcfa876d394bfe1d85b
Hoang Linh <lehoanglinh1051995@gmail.com>
2024-07-03 14:34:29 +0700

    fix: refactor hw 45

--- Nodejs/45_swagger/app.js
+++ Nodejs/45_swagger/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const mysql = require('mysql2');
+const cors = require('cors');
+const helmet = require('helmet');
+
+
 const swagger = require('./swagger');
+
 const usersRoutes = require('./routes/users');
 const hotelsRoutes = require('./routes/hotels');
 const reviewsRoutes = require('./routes/reviews');
@@ -9,7 +14,9 @@ const rooms_bookingRoutes = require('./routes/rooms_booking');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
 
 app.use('/users', usersRoutes);
 app.use('/hotels', hotelsRoutes);
@@ -17,9 +24,7 @@ app.use('/rooms/booking', rooms_bookingRoutes);
 app.use('/reviews', reviewsRoutes);
 
 
-swagger(app)
-
-
+swagger(app);
 
 app.listen(PORT, () => {
     console.log(`Listen on port: ${PORT}`);
