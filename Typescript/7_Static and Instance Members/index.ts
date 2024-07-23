bac888ea9fa414b65012c0351105379022ae4b20
Hoang Linh <lehoanglinh1051995@gmail.com>
2024-07-23 08:52:47 +0700

    ft: add homework 68 Static and Instance Members

---
+++ Typescript/7_Static and Instance Members/index.ts
@@ -0,0 +1,26 @@
+class MyClass {
+    static count: number = 0;
+
+    static displayStaticCount() {
+        console.log(`Static Count: ${MyClass.count}`);
+    }
+
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+        MyClass.count += 1;
+    }
+
+}
+
+
+let instance1 = new MyClass("Instance1");
+// instance1.instanceCount = 1;
+
+MyClass.displayStaticCount();
+
+let instance2 = new MyClass("Instance2");
+
+MyClass.displayStaticCount();
+
